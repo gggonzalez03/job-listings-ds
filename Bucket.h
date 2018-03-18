5dadcf2802aaf8566b1be6577313f49fadc625a1
FawzanKhan <fawzankhan6@gmail.comm>
2018-03-18 00:16:32 -0700

    new branch commit

---
+++ Bucket.h
@@ -0,0 +1,98 @@
+#ifndef BUCKET_H
+#define BUCKET_H
+
+#include "HashEntry.h"
+
+const int BUCKET_SIZE = 3;
+
+template<typename K, typename V>
+class Bucket{
+private:
+  HashEntry<K,V> **bucketArray;
+  int count;
+public:
+  Bucket();
+  ~Bucket();
+  int getCount();
+  V searchBucketArray(K);
+  void insertBucketArray(K,V);
+  V removeBucketArray(K);
+};
+
+template<typename K, typename V>
+Bucket<K,V>::Bucket() {
+  bucketArray = new HashEntry<K,V>*[BUCKET_SIZE];
+  count = 0;
+}
+
+template<typename K, typename V>
+Bucket<K,V>::~Bucket() {
+  for(int i = 0; i < BUCKET_SIZE; i++) {
+    if(bucketArray[i] != NULL) {
+      delete bucketArray[i];
+    }
+  }
+  delete[] bucketArray;
+}
+
+template<typename K, typename V>
+int Bucket<K,V>::getCount() {
+  return count;
+}
+
+template<typename K, typename V>
+V Bucket<K,V>::searchBucketArray(K key) {
+  int index = 0;
+  if(bucketArray[index]->getKey() == key) {
+    return bucketArray[index]->getValue();
+  } else if(bucketArray[index + 1]->getKey() == key) {
+    return bucketArray[index + 1]->getValue();
+  } else if(bucketArray[index + 2]->getKey() == key) {
+    return bucketArray[index + 2]->getValue();
+  }
+  return NULL;
+}
+
+template<typename K, typename V>
+void Bucket<K,V>::insertBucketArray(K key, V value) {
+  int index = 0;
+  // if empty index is at 0
+  if(bucketArray[index] == NULL) {
+    HashEntry<K,V> *temp = new HashEntry<K,V>(key value);
+    bucketArray[index] = temp;
+    count++;
+  } else if(bucketArray[index + 1] == NULL) { //if empty index is at 1
+    HashEntry<K,V> *temp = new HashEntry<K,V>(key value);
+    bucketArray[index] = temp;
+    count++;
+  } else if(bucketArray[index + 2] == NULL) { // if empty index is at 2
+    HashEntry<K,V> *temp = new HashEntry<K,V>(key value);
+    bucketArray[index] = temp;
+    count++;
+  }
+}
+
+template<typename K, typename V>
+V Bucket<K,V>::removeBucketArray(K key) {
+  int index = 0;
+  // if item is at index[0] of bucketArray
+  if(bucketArray[index]->getKey() == key) {
+    HashEntry<K,V> *temp = bucketArray[index];
+    //bucketArray[index] = NULL;
+    count--;
+    return temp->getValue();
+  } else if(bucketArray[index + 1]->getKey() == key) { // if item is at index[1]
+    HashEntry<K,V> *temp = bucketArray[index];
+    //bucketArray[index] = NULL;
+    count--;
+    return temp->getValue();
+  } else if(bucketArray[index + 2]->getKey() == key) { // if item is at index[2]
+    HashEntry<K,V> *temp = bucketArray[index];
+    //bucketArray[index] = NULL;
+    count--;
+    return temp->getValue();
+  }
+  return NULL;
+}
+
+#endif
--- HashEntry.h
+++ HashEntry.h
@@ -2,23 +2,28 @@
 #ifndef HASH_ENTRY
 #define HASH_ENTRY
 
+#include "Bucket.h"
+
 template<typename K, typename V>
 class HashEntry {
 private:
   K key;
   V value;
+  Bucket<K,V> **bucketArr;
 public:
   // Constructor
   HashEntry(K, V);
   // Getters
   K getKey();
   V getValue();
+  *Bucket<K,V> getBucketArr();
 };
 
 template<typename K, typename V>
 HashEntry<K,V>::HashEntry(K key, V value) {
   this->key = key;
   this->value = value;
+  bucketArr = new Bucket<K,V>*();
 }
 
 template<typename K, typename V>
@@ -31,4 +36,8 @@ V HashEntry<K, V>::getValue() {
   return value;
 }
 
+template<typename K, typename V>
+*Bucket<K,V> HashEntry<K,V>::getBucketArr() {
+  return bucketArr;
+}
 #endif
