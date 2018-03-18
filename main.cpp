5d983671e204658db816251e1e6fb4161d0e83be
FawzanKhan <fawzankhan6@gmail.comm>
2018-03-18 02:17:30 -0700

    updated main, look for comments

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <cstdlib>
 #include <string>
 #include <iomanip>
+#include <ctime>
 
 #include "Job.h"
 #include "BinarySearchTree.h"
@@ -45,7 +46,7 @@ bool compareID(const Job &, const Job &);
 bool compareDate(const Job &, const Job &);
 void printIndentedItem(int depth, Job &job);
 
-int generateID();
+int generateID(HashTable<int, Job> &hashTable);
 int getTodaysDate();
 
 void readJobsIntoBinarySearchTree(BinarySearchTree<Job> &jobs, string fileName);
@@ -54,33 +55,33 @@ void readJobsIntoArray(Job *jobs, string fileName);
 
 int main() {
     string choice;
-    
+
     // Unsorted list
     Job *unsortedJobs = new Job[25];
-    
+
     // List of jobs that is based on the primary key (unique id)
     BinarySearchTree<Job> *jobs = new BinarySearchTree<Job>(compareID);
-    
+
     // List of jobs that is based on the secondary key (date)
     BinarySearchTree<Job> *jobs2 = new BinarySearchTree<Job>(compareDate);
-    
+
     // The hash table for the primary tree
     HashTable<int, Job> *hashTable = new HashTable<int, Job>();
-    
-    
+
+
     readJobsIntoArray(unsortedJobs, "jobs.txt");
-    
+
     // read the jobs into the trees
     readJobsIntoBinarySearchTree(*jobs, "jobs.txt");
     readJobsIntoBinarySearchTree(*jobs2, "jobs.txt");
-    
+
     while (*choice.c_str() != 'L')
     {
         displayMenu();
-        
+
         cout << "Enter choice: ";
         getline(cin, choice);
-        
+
         switch(*choice.c_str())
         {
             case 'D':
@@ -133,7 +134,7 @@ void printIndentedItem(int depth, Job &job)
     for (int i = 0; i < depth; i++) {
         cout << "   ";
     }
-    
+
     cout << depth << ". ";
     display(job);
     cout << endl;
@@ -151,15 +152,15 @@ void displayMenu()
 void displayJobListings(BinarySearchTree<Job> &jobs, BinarySearchTree<Job> &jobs2)
 {
     string choice = "";
-    
+
     cout << "U - Print unsorted job list" << endl;
     cout << "P - Print jobs sorted by ID" << endl;
     cout << "S - Print jobs sorted by date" << endl;
     cout << "I - Print jobs as an indented list" << endl;
-    
+
     cout << "Enter choice: ";
     getline(cin, choice);
-    
+
     switch (*choice.c_str()) {
         case 'U':
 //            printUnsorted();
@@ -181,13 +182,13 @@ void displayJobListings(BinarySearchTree<Job> &jobs, BinarySearchTree<Job> &jobs
 void search(BinarySearchTree<Job> &jobs2, HashTable<int, Job> &hashTable)
 {
     string choice = "";
-    
+
     cout << "P - Search job by ID" << endl;
     cout << "SK - Search job by date" << endl;
-    
+
     cout << "Enter choice: ";
     getline(cin, choice);
-    
+
     if (choice == "P")
         searchById(hashTable);
     else if (choice == "SK")
@@ -202,37 +203,37 @@ void search(BinarySearchTree<Job> &jobs2, HashTable<int, Job> &hashTable)
 void searchById(HashTable<int, Job> &hashTable)
 {
     string id = "";
-    
+
     cout << "Enter 5 digit ID (ex. 12423): ";
     getline(cin, id);
-    
+
     cout << "Search the list using " << id << " " << endl;
 }
-
+// updated by Fawzan
 void searchByDate(BinarySearchTree<Job> &jobs2)
 {
     string date = "";
-    
+
     cout << "Enter date (mm/dd/yyy): ";
     getline(cin, date);
-    
-    // TODO:
+
     // Convert date to integer
-    
+    int searchDate = stoi(date);
+
     cout << "Search the list by date: " << date;
 }
 
 void add(BinarySearchTree<Job> &jobs, BinarySearchTree<Job> &jobs2, HashTable<int, Job> &hashTable)
 {
     string choice = "";
-    
+
     cout << "J - Add single entry" << endl;
     cout << "F - Add listings using a file" << endl;
-    
+
     cout << "Enter choice: ";
     getline(cin, choice);
 
-    
+
     if (choice == "J")
         addJob(jobs, jobs2, hashTable);
     else if (choice == "F")
@@ -243,34 +244,49 @@ void add(BinarySearchTree<Job> &jobs, BinarySearchTree<Job> &jobs2, HashTable<in
         return;
     }
 }
-
+// updated by Fawzan
 void addJob(BinarySearchTree<Job> &jobs, BinarySearchTree<Job> &jobs2, HashTable<int, Job> &hashTable)
 {
     string entry = "";
     cout << "Enter job information (ex. Title Company City): ";
     getline(cin, entry);
-    
+
     // TODO:
     // Take the input and concatenate such that these
     // variables are set appropriately
     string title = "";
     string company = "";
     string city = "";
-    
-    // TODO:
-    
-    // int id = generateID();
-    
-    // int date = getTodaysDate();
-    
-    // TODO:
+
+    /****** Fawzan's Suggestion
+    string title = "";
+    string company = "";
+    string city = "";
+
+    cout << "Enter job information. Title: ";
+    cin >> title;
+
+    cout <<"\nCompany: ";
+    cin >> company;
+
+    cout << "\nCity: ";
+    cin >> city;
+    *******/
+
+    // both generateID and getTodaysDate are ready to be implemented
+    int id = generateID(hashTable);
+
+    int date = getTodaysDate();
+
     // Create Job class object using values from the user and generated id and date
-    // Job newJob = new Job(id, title, date, city);
-    
-    // TODO:
+     Job newJob = new Job(id, title, company, date, city);
+
     // Call add node function from BST to actually add the entry into the tree (primary tree)
+    jobs->insert(newJob);
     // Call add node function for secondary tree
+    jobs2->insert(newJob);
     // Call insert to Hash Table
+    hashTable->insertGoodHash(newJob->getID(), newJob);
 }
 
 void addJobs(BinarySearchTree<Job> &jobs, BinarySearchTree<Job> &jobs2, HashTable<int, Job> &hashTable)
@@ -278,10 +294,10 @@ void addJobs(BinarySearchTree<Job> &jobs, BinarySearchTree<Job> &jobs2, HashTabl
     string fileName = "";
     cout << "Enter the name of the txt file containing the job listings: ";
     getline(cin, fileName);
-    
+
     readJobsIntoBinarySearchTree(jobs, fileName);
     readJobsIntoBinarySearchTree(jobs2, fileName);
-    
+
     // TODO:
     // Add to hash table
 }
@@ -289,14 +305,14 @@ void addJobs(BinarySearchTree<Job> &jobs, BinarySearchTree<Job> &jobs2, HashTabl
 void del(BinarySearchTree<Job> &jobs, BinarySearchTree<Job> &jobs2, HashTable<int, Job> &hashTable)
 {
     string choice = "";
-    
+
     cout << "J - Add single entry" << endl;
     cout << "L - Add listings using a file" << endl;
-    
+
     cout << "Enter choice: ";
     getline(cin, choice);
 
-    
+
     if (choice == "J")
         deleteJob(jobs, jobs2, hashTable);
     else if (choice == "L")
@@ -311,14 +327,14 @@ void del(BinarySearchTree<Job> &jobs, BinarySearchTree<Job> &jobs2, HashTable<in
 void deleteJob(BinarySearchTree<Job> &jobs, BinarySearchTree<Job> &jobs2, HashTable<int, Job> &hashTable)
 {
     string id = "";
-    
+
     cout << "Enter job id to delete: ";
     getline(cin, id);
-    
+
     // TODO:
     // Delete in BST (Primary and Secondary)
     // Delete in Hash table
-    
+
     // This could be inside if(deleteEntry(id)) where deleteEntry() returns boolean
     cout << "Deleted successfully" << endl;
 }
@@ -342,14 +358,14 @@ bool login()
 {
     string un;
     string pw;
-    
+
     // Ask user for pw and username
     cout << "Enter username: ";
     getline(cin, un);
     cout << "Enter password: ";
     getline(cin, pw);
-    
-    
+
+
     if (un == username && pw == password)
     {
         return true;
@@ -359,33 +375,33 @@ bool login()
         cout << "Please enter valid username and password." << endl;
         login();
     }
-    
+
     return false;
 }
 
 void readJobsIntoBinarySearchTree(BinarySearchTree<Job> &jobs, string fileName)
 {
-    
+
     string title = "";
     string company = "";
     string city = "";
     int id = 0;
     int date = 0;
-    
+
     ifstream infile;
     infile.open(fileName);
-    
+
     while(!infile)
     {
         cout << "Error opening " << fileName << " for reading\n";
         exit(111);
     }
-    
+
     // TODO:
     // Read the file and insert items into the primaryTree, secondaryTree, and the hast table
     cout << "Reading file " << fileName << endl;
     Job *job = nullptr;
-    
+
     while(infile >> id)
     {
         infile.ignore();
@@ -396,17 +412,17 @@ void readJobsIntoBinarySearchTree(BinarySearchTree<Job> &jobs, string fileName)
         getline(infile, city, ';');
         infile.ignore();
         infile >> date;
-        
+
         // Create a Job object
         job = new Job(id, title, company, date, city);
-        
+
         // Insert the object
         jobs.insert(*job);
-        
+
         // TODO:
         // Insert in hash
         // Insert in secondary tree (sorted by date)
-        
+
     }
     infile.close();
 }
@@ -418,22 +434,22 @@ void readJobsIntoArray(Job *jobs, string fileName)
     string city = "";
     int id = 0;
     int date = 0;
-    
+
     ifstream infile;
     infile.open(fileName);
-    
+
     while(!infile)
     {
         cout << "Error opening " << fileName << " for reading\n";
         exit(111);
     }
-    
+
     // TODO:
     // Read the file and insert items into the primaryTree, secondaryTree, and the hast table
     cout << "Reading file " << fileName << endl;
     Job *job = nullptr;
     int index = 0;
-    
+
     while(infile >> id)
     {
         infile.ignore();
@@ -444,14 +460,50 @@ void readJobsIntoArray(Job *jobs, string fileName)
         getline(infile, city, ';');
         infile.ignore();
         infile >> date;
-        
+
         // Create a Job object
         job = new Job(id, title, company, date, city);
-        
+
         // Insert the address
         *(jobs + index) = *job;
-        
+
         index++;
     }
     infile.close();
+}
+//updated by Fawzan
+// Generates 4 digit ID and searches HashTable to see if ID already exists
+int generateID(HashTable<int, Job> &hashTable) {
+  srand(time(NULL));
+  int newID = 0;
+  newID = rand() % 2000 + 1000;
+  while(hashTable->searchTable(newID) != NULL) {
+      newID = rand() % 2000 + 1000;
+  }
+  return newID;
+}
+//updated by Fawzan
+// Generates the current days date (Today)
+int getTodaysDate() {
+  int day = 0;
+  int month = 0;
+  int year = 0;
+
+  time_t now = time(0);
+  struct tm* ptr2tm;
+  ptr2tm = localtime(&now);
+
+  day = ptr2tm->tm_mday;
+  month = ptr2tm->tm_mon+1;
+  year = ptr2tm->tm_year+1900;
+
+  string d = to_string(day);
+  string m = to_string(month);
+  string y = to_string(year);
+  string z = "0";
+  string wholeDate = y + z + m + d;
+
+  int todaysDate = stoi(wholeDate);
+
+  return todaysDate;
 }
