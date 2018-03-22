cb4014ceedfc7af977172c0f51b7b68a7d99f389
FawzanKhan <fawzankhan6@gmail.comm>
2018-03-22 10:02:38 -0700

    fixed linked list

--- LinkList.h
+++ LinkList.h
@@ -2,98 +2,111 @@
 #define LINKLIST_H
 
 #include "Node.h"
+using std::cout;
 
 template<typename K, typename V>
 class LinkList {
 private:
-  Node* top;
-  int count;
+    Node<K,V>* top;
+    int count;
 public:
-  LinkList();
-  ~LinkList();
-  Node* find(K);
-  void add(K,V);
-  bool remove(K);
-  Node* getTop();
+    LinkList();
+    ~LinkList();
+    Node<K,V>* find(K);
+    void add(K,V);
+    bool remove(K);
+    Node<K,V>* getTop();
+    void print();
 };
 
 template<typename K, typename V>
 LinkList<K,V>::LinkList() {
-  top(NULL,NULL);
-  count = 0;
+    top = NULL;
+    count = 0;
 }
 
 template<typename K, typename V>
 LinkList<K,V>::~LinkList() {
-  Node* temp = top;
-  while (temp != nullptr)
-  {
-    top = top -> get_next();
-    delete temp;
-    temp = top;
-  }
+    Node<K,V>* temp = top;
+    while (temp != nullptr)
+    {
+        top = top -> get_next();
+        delete temp;
+        temp = top;
+    }
 }
 
 template<typename K, typename V>
-Node* LinkList<K,V>::find(K key) {
-  Node* temp = top;
-  while (temp != 0)
-  {
-    if (temp->getK() == key) return temp;
-    temp = temp -> get_next();
-  }
-  return 0;
+Node<K,V>* LinkList<K,V>::find(K key) {
+    Node<K,V>* temp = top;
+    while (temp != 0)
+    {
+        if (temp->getK() == key) return temp;
+        temp = temp -> getNext();
+    }
+    return 0;
 }
 
 template<typename K, typename V>
 void LinkList<K,V>::add(K key, V value) {
-  Node* newNode = new Node(key, value);
-  if(count == 0) {
-    top = newNode;
-    count++;
-  } else {
-    Node* currNode = top;
-    while(currNode->getNext() != NULL) {
-      currNode = currNode->getNext();
+    Node<K,V>* newNode = new Node<K,V>(key, value);
+    if(count == 0) {
+        top = newNode;
+        count++;
+    } else {
+        Node<K,V>* currNode = top;
+        while(currNode->getNext() != NULL) {
+            currNode = currNode->getNext();
+        }
+        currNode->setNext(newNode);
+        count++;
     }
-    currNode->setNext(newNode);
-    count++;
-  }
 }
 
 template<typename K, typename V>
 bool LinkList<K,V>::remove(K key) {
-  if(!find(key)) {
+    if(!find(key)) {
+        return false;
+    }
+    Node<K,V>* temp = top;
+    Node<K,V>* temp2 = NULL;
+    Node<K,V>* temp3 = NULL;
+
+    while(temp->getK() != key) {
+        temp2 = temp;
+        temp = temp->getNext();
+    }
+    if(temp == top) {
+        temp3 = temp;
+        temp = temp->getNext();
+        delete temp3;
+        return true;
+    } else if(temp->getNext() == NULL) {
+        temp2->setNext(NULL);
+        delete temp;
+        return true;
+    } else {
+        Node<K,V>* temp4 = temp;
+        temp = temp->getNext();
+        temp2->setNext(temp);
+        delete temp4;
+        return true;
+    }
     return false;
-  }
-  Node* temp = top;
-  Node* temp2 = NULL;
-  Node* temp3 = NULL;
-  while(temp->getK() != key) {
-    temp2 = temp;
-    temp = temp->get_next();
-  }
-  if(temp == top) {
-    temp3 = temp;
-    temp = temp->get_next();
-    delete temp2;
-    return true;
-  } else if(temp->get_next() == NULL) {
-    temp2->set_next(NULL);
-    delete temp;
-    return true;
-  } else {
-    Node* temp4 = temp;
-    temp = temp->get_next();
-    temp2->set_next(temp);
-    delete temp4;
-    return true;
-  }
 }
 
 template<typename K, typename V>
-Node* LinkList<K,V>::getTop() {
-  return top;
+Node<K,V>* LinkList<K,V>::getTop() {
+    return top;
+}
+
+template<typename K, typename V>
+void LinkList<K,V>::print() {
+    Node<K,V>* temp = getTop();
+    while(temp != NULL) {
+        cout << temp->getV();
+        temp = temp->getNext();
+    }
 }
 
 #endif
